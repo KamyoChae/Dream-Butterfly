14f64a48e9662c5399864461a933bb03e5511e68
kamyochae <1465678596@qq.com>
2019-03-09 18:42:08 +0800

    调整碰撞检测效果

--- src/js/ModelControl.js
+++ src/js/ModelControl.js
@@ -179,10 +179,10 @@ class ModelControl {
 
             if (dec < -5) {
                 show.innerHTML = "往左" + `${dec}`
-                that.speed = -2
+                that.speed = -1
             } else if (dec > 5) {
                 show.innerHTML = "往右" + `${dec}`
-                that.speed = 2
+                that.speed = 1
             } else {
                 show.innerHTML = "水平" + `${dec}`
                 that.speed = 0
